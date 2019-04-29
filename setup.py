c56c3ae80a13d3c453e5734046beb028f25a7813
Seb-Good <sebi.goodfellow@gmail.com>
2019-04-29 11:44:26 -0400

    -Added notebook for dataset generation.

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Setup for MNIST-Azure"""
+from setuptools import setup
+
+setup(
+    name='mnist-azure',
+    version='0.0.1',
+    description='Productionization of MNIST Tensorflow model using Azure Machine Learning Service',
+    url='https://github.com/Seb-Good/mnist-azure.git',
+    author='Sebastian D. Goodfellow, Ph.D.',
+    license='MIT',
+    keywords='deep learning',
+    package_dir={'': 'mnist-azure'},
+    zip_safe=False,
+)
